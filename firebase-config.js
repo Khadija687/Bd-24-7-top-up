f5573e6c1e7d614479d86833132ba4444eb61586
Khadija687 <najokhasan77@gmail.com>
2025-04-17 15:54:00 +0600

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,20 @@
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCPHxnRxoV81eqMYfHahbPCbDwdGvDRLHI",
+  authDomain: "ggweb-d6cdf.firebaseapp.com",
+  projectId: "ggweb-d6cdf",
+  storageBucket: "ggweb-d6cdf.firebasestorage.app",
+  messagingSenderId: "734632280776",
+  appId: "1:734632280776:web:76ee0af2c4773929b91bfd",
+  measurementId: "G-5GFXLGLTNT"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
